d06257e6b13e1589133357cd669fbc6b0009982f
Pascal Veraguth <pascal.veraguth@hotmail.ch>
2024-10-18 15:36:14 +0200

    Implement SQLite support, update Entity classes with JPA annotations, add Entities relationships

--- src/main/java/ch/csbe/bibliotheksapp_modul426/resources/entities/Reminder.java
+++ src/main/java/ch/csbe/bibliotheksapp_modul426/resources/entities/Reminder.java
@@ -1,10 +1,22 @@
 package ch.csbe.bibliotheksapp_modul426.resources.entities;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
+@Entity
 @Data
 public class Reminder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     private int loanId; // Fremdschlüssel
+
     private boolean emailSent;
+
+    // Mehrere reminders können zu einer Ausleihe gehören
+    @ManyToOne
+    @JoinColumn(name = "loan_id", nullable = false)
+    private Loan loan;
 }

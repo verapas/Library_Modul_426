0168940b8b1ea8b44bafa09ad45399da81f511b1
D4rkNexus <adem.malushaj@ict.csbe.ch>
2024-10-19 14:11:54 +0200

    Refracted files, Repository changed

--- src/main/java/ch/csbe/bibliotheksapp_modul426/resources/serviceLayer/BookService.java
+++ src/main/java/ch/csbe/bibliotheksapp_modul426/resources/serviceLayer/BookService.java
@@ -1,6 +1,7 @@
-package ch.csbe.bibliotheksapp_modul426.resources.entities.serviceLayer;
+package ch.csbe.bibliotheksapp_modul426.resources.serviceLayer;
 
 import ch.csbe.bibliotheksapp_modul426.resources.entities.Book;
+import ch.csbe.bibliotheksapp_modul426.resources.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
--- src/main/java/ch/csbe/bibliotheksapp_modul426/resources/serviceLayer/UserService.java
+++ src/main/java/ch/csbe/bibliotheksapp_modul426/resources/serviceLayer/UserService.java
@@ -1,4 +1,4 @@
-package ch.csbe.bibliotheksapp_modul426.resources.entities.serviceLayer;
+package ch.csbe.bibliotheksapp_modul426.resources.serviceLayer;
 
 import ch.csbe.bibliotheksapp_modul426.resources.entities.User;
 import ch.csbe.bibliotheksapp_modul426.resources.repository.UserRepository;
